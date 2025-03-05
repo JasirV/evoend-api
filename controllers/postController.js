70c860a0edc30aa34b3b246bf14b17e7d5d0c5ec
JasirV <jasirmon133@gmail.com>
2025-03-06 01:16:11 +0530

    add create update delete get in post

--- controllers/postController.js
+++ controllers/postController.js
@@ -18,6 +18,44 @@ const createPost = async (req, res) => {
   }
 };
 
+const UpdatePost=async (req,res)=>{
+    try {
+        const {id}=req.params
+        const {title,description}=req.body
+        if(!id){
+            return res.status(404).json({status:"failed",message:"file thie requiredments"})
+        }
+        if(!title||!description){
+            return res.status(404).json({status:'failed',mesage:"required file the "})
+        }
+        const updatepost=await Post.findByIdAndUpdate(id,{title,description},{new:true})
+        if(!updatepost){
+            return res.status(404).json({status:'failed',message:"Post not font"}) 
+        }
+        return res.status(200).json({status:'succees',message:"post update success fully"})
+    } catch (error) {
+        return res.status(500).json({status:"failed",message:"Internal server error",error:error.message})
+    }
+}
+
+const getPost =async (req,res)=>{
+    try {
+        const {id}=req.params
+        if(!id){
+            const posts=await Post.find()
+            return res.status(200).json({status:"success",message:'Post alll fetched',data:posts})
+        }
+        const post=await Post.findOne(id)
+        if(!post){
+            return res.status(404).json({status:'failed',message:"Post not font"})
+        }
+        return res.status(200).json({status:"success",message:"post sucecss fully fecthed",data:post})
+        
+    } catch (error) {
+        return res.status(500).json({status:"failed",message:"Internal server error",error:error.mesage})
+    }
+}
+
 const deletePost =async (req,res)=>{
     try {
         const {id}=req.params
@@ -25,7 +63,7 @@ const deletePost =async (req,res)=>{
             return res.status(400).json({status:'failed',message:"Missing required fields"})
         }
         const deletepost=await Post.findByIdAndDelete(id)
-        if(!deletePost){
+        if(!deletepost){
             return res.status(404).json({status:'failed',mesage:'Post not found'})
         }
         return res.status(200).json({status:"success",mesage:"Post Success Fully Deleted"})
@@ -34,4 +72,4 @@ const deletePost =async (req,res)=>{
     }
 }
 
-module.exports = createPost;
+module.exports = {createPost,getPost,UpdatePost,deletePost};
